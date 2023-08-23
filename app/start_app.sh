516aa3c52d543530b906162b08d0165614899d46
Matias Garafoni <matias.garafoni@gmail.com>
2023-08-23 02:26:52 -0300

    fixed issue with port 80 not working

--- app/start_app.sh
+++ app/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alembic upgrade head || exit 1
 # python tests.py || exit 1
-gunicorn --bind 0.0.0.0:5050 --certfile /etc/letsencrypt/live/<your_domain>/fullchain.pem --keyfile /etc/letsencrypt/live/<your_domain>/privkey.pem app:app 
+gunicorn --bind 0.0.0.0:5050 -w 3 app:app 
